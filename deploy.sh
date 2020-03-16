9d47c953110b2de3e86260dd9221bdefd1d00e2a
hollson <hollson@qq.com>
2020-03-16 18:44:29 +0800

    rebuilding site 2020年 3月16日 星期一 18时44分29秒 CST

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Build the project.
+# Hugo编译
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# Go To Public folder
-cd public
-# Add changes to git.
+# 推送源码
 git add .
-
-# Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-
-# Push source and build repos.
 git push origin master
 
-# Come Back up to the Project Root
+# 推送发布
+cd public
+git add .
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+git push origin master
 cd ..
